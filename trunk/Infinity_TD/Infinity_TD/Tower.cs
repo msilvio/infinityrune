0e9c85511efaad655a3a6125dfc43e84f117a0df
rodreiser@gmail.com <rodreiser@gmail.com@4172a913-26c5-4b38-7b6f-fcb302f8a7c0>
2012-01-28 15:59:15 +0000

    torres

--- trunk/Infinity_TD/Infinity_TD/Tower.cs
+++ trunk/Infinity_TD/Infinity_TD/Tower.cs
@@ -2,10 +2,44 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
-
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
 namespace Infinity_TD
 {
     class Tower
     {
+        private float dmg
+        {
+            get;
+            set;
+        }
+
+        private Vector2 pos;
+        private Texture2D textura;
+        private const int RANGE = 4;
+
+        private float fireRate
+        {
+            get;
+            set;
+        }
+
+        public Tower(Texture2D _textura, float _dmg, Vector2 _pos, float _fireRate)
+        {
+            dmg = _dmg;
+            textura = _textura;
+            fireRate = _fireRate;
+            pos = _pos;
+
+        }
+
+        public virtual void Draw(SpriteBatch sb)
+        {
+            sb.Draw(textura, pos, Color.Wheat);
+        }
+        public void Update()
+        {
+
+        }
     }
 }
---
+++ trunk/Infinity_TD/Infinity_TD/Towers/WaterTower.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Content;
+using Microsoft.Xna.Framework.Graphics;
+
+namespace Infinity_TD.Towers
+{
+    class WaterTower : Tower
+    {
+        public WaterTower(Texture2D _textura, float _dmg, Vector2 _pos, float _fireRate):base( _textura,  _dmg, _pos, _fireRate) { 
+        
+        }
+    }
+}
